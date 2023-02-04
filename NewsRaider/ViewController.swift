c025569810466b7b7a0da6f9c42f736b494a84f8
Danil <akimov.dvp@yandex.ru>
2023-02-04 12:18:03 +0300

    Initial Commit

---
+++ NewsRaider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsRaider
+//
+//  Created by Данил Акимов on 04.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
